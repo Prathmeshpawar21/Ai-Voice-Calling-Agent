494d5ade2286e97eaecb0c9e244c48a58215bb97
Prathamesh J. Pawar <prathmeshpawar212002@gmail.com>
2024-12-13 18:59:21 +0530

    CMD from

---
+++ app.py
@@ -0,0 +1,16 @@
+import assemblyai as asi
+# `pip install assemblyai` (Windows)
+
+import assemblyai as aai
+
+aai.settings.api_key = "04798c6e7a304a249b3ebb9f66461bcc"
+transcriber = aai.Transcriber()
+
+transcript = transcriber.transcribe("https://assembly.ai/news.mp4")
+# transcript = transcriber.transcribe("./my-local-audio-file.wav")
+
+print(transcript.text)
+
+
+# Prathmeshpawar21
+# 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
